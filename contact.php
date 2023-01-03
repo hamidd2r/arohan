6fc1d6c08b9ba7fc2da1f79adca21edb1023cdec
Hamid <ali.hamid161999@gmail.com>
2023-01-03 13:05:33 +0530

    add contact

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
+
 if (isset($_POST['submit'])) {
-    echo 'work';
-    die();
+    
     $to = $_POST['email']; // this is your Email address
     $from = 'ali.hamid161999@gmail.com'; // this is the sender's Email address
     $first_name = $_POST['first_name'];
@@ -29,10 +29,8 @@ if (isset($_POST['submit'])) {
     $headers2 = 'From:' . $to;
     mail($to, $subject, $message, $headers);
     mail($from, $subject2, $message2, $headers2); // sends a copy of the message to the sender
-    echo 'Mail Sent. Thank you ' .
-        $first_name .
-        ', we will contact you shortly.';
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
+    //echo 'Mail Sent. Thank you ' .$first_name .', we will contact you shortly.';
+    echo '<script>alert("Mail Sent. Thank you ' .$first_name .', we will contact you shortly.")</script>';
+    header('Location:contact.html'); 
 }
 ?>
